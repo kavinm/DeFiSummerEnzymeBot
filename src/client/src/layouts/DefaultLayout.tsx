b2b6ec9ee2f655835857bafa447873b65a14860b
Javan <javan510.gmail.com>
2021-07-26 20:41:21 -0700

    footer layout

--- src/client/src/layouts/DefaultLayout.tsx
+++ src/client/src/layouts/DefaultLayout.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Flex, Box } from '@chakra-ui/react';
-import { Sidebar, Topbar } from '../components/shared';
+import { Sidebar, Topbar,Footer } from '../components/shared';
 
 const DefaultLayout: React.FC<{ name: string }> = ({ children, name }) => {
   return (
     <Flex minHeight="100vh">
       <Sidebar />
-      <Box minHeight="100vh" backgroundColor="accentSurface" w="calc(100vw - 230px)">
+      <Box minHeight="100vh" backgroundColor="accentSurface" w="calc(100vw - 230px)" position="relative">
         <Topbar name={name} />
         {children}
+        <Footer />
       </Box>
+      
     </Flex>
   );
 };
